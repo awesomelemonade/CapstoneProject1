e42519faea6bbb26894dad4fa512565e4818ba28
Edison Situ <situe@bxscience.edu>
2018-07-12 15:54:47 -0400

    Updated dig_to_spec function cause me dumb

--- Digital_To_Spectrum.py
+++ Digital_To_Spectrum.py
@@ -1,14 +1,8 @@
 def dig_to_spec(data, fs):
-    fig, ax = plt.subplots()
-    S, freqs, times = m.specgram(data, NFFT=4096, Fs=fs,
+    return mlab.specgram(data, NFFT=4096, Fs=fs,
                                   window=mlab.window_hanning,
                                   noverlap=4096 // 2)
-    fig.colorbar(im)
-    ax.set_xlabel("Time (sec)")
-    ax.set_ylabel("Frequency (Hz)")
-    ax.set_title("Spectrogram of Trumpet")
-    ax.set_ylim(0, 6000);
-    return S
+                            
 
 def spec_to_peaks(data, value, fp = generate_binary_structure(rank = 2, connectivity=2)):
     max_arr = maximum_filter(data, footprint = fp)
