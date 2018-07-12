5f01bc99bb8c0d1dbfa2fc0e2c4a7158367b8a05
awesomelemonade <awesomelemonade@gmail.com>
2018-07-12 15:07:25 -0400

    Added Databasing

---
+++ databasing.py
@@ -0,0 +1,41 @@
+
+import pickle
+class AudioDatabase:
+	def __init__(self, filename):
+		self.filename = filename;
+		self.dictionary = {}
+	def put(self, key, value):
+		if key not in self.dictionary:
+			self.dictionary[key] = []
+		self.dictionary[key].append(value)
+	def get(self, key):
+		return self.dictionary.get(key, [])
+	def save(self):	
+		with open(self.filename, mode="wb") as file:
+			pickle.dump(self.dictionary, file)
+	def load(self):
+		with open(self.filename, mode="rb") as file:
+			self.dictionary = pickle.load(file)
+
+def store(fingerprints, song):
+	for fingerprint in fingerprints:
+		database.put((fingerprint.frequency1, fingerprint.frequency2, fingerprint.delta), (song, fingerprint.time))
+
+from collections import Counter, namedtuple
+
+def predict(database, fingerprints):
+	counter = Counter()
+	for fingerprint in fingerprints:
+		matches = database.get((fingerprint.frequency1, fingerprint.frequency2, fingerprint.delta))
+		for match in matches:
+			counter.update((match[0], match[1] - fingerprint.time))
+	return counter.most_common()[0]
+
+Fingerprint = namedtuple('Fingerprint', ['frequency1', 'frequency2', 'delta', 'time'])
+
+database = AudioDatabase("database.whydoyoucareaboutthisextension")
+database.put("HIIII", "BLABLA")
+database.save()
+fingerprint = Fingerprint(0, 1, 2, 3)
+print(fingerprint)
+
