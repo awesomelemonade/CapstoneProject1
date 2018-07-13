d605cb8ecf3f5382356341bac432c0a5f2539849
New Annabelle <lewa@bxscience.edu>
2018-07-13 14:20:24 -0400

    fixed bugs

--- peaks_to_fingerprints.py
+++ peaks_to_fingerprints.py
@@ -24,18 +24,18 @@ def peaks_to_fingerprints(peaks, fanout):
     # gets two arrays, frequency and time. Use np.where on peaks
     times, freqs = np.where(peaks.T)
 
-    frequency_index = np.arange(len(freqs)) # makes an array for every index in data(i: 0,1,2,3,4,5,6)
-    index_chart = frequency_index[:-1].reshape(-1, 1) + (np.arange(fanout) + 1) # makes a chart of all the indexes, row = f2 values associated with f1 (col)
-    index_chart = index_chart[index_chart < len(frequency_index)] # deletes indicies that don't exist
-
     # this was like 90% edison so creds to him
-    ones = np.ones(fanout, dtype=int)
-    # f1 = (freqs.reshape(-1,1) * ones)[:index_chart].flatten()[:len(index_chart)] # frequency 1
-    print((freqs.reshape(-1, 1) * ones)[:, :index_chart])
-    f2 = freqs[index_chart].flatten() # frequency 2
-    t1 = (times.reshape(-1,1) * ones[:index_chart]).flatten()[:len(index_chart)] # time 1
-    delta_t = (times[index_chart].flatten() - t1) # change between time 2 and time 1
+    frequency_index = np.arange(len(freqs)) # makes an array for every index in data(i: 0,1,2,3,4,5,6)
+    index_chart = (frequency_index.reshape(-1,1) + (np.arange(fanout) + 1)).flatten() # makes a chart of all the indexes, row = f2 values associated with f1 (col)
+    index_chart2 = index_chart[index_chart < len(frequency_index)] # deletes indicies that don't exist
+    index_chart[index_chart >= len(frequency_index)] = -1 #reusing index chart to get out of bounds to be negative
+
+    ones = np.ones(fanout)
+    f1 = (freqs.reshape(-1,1) * ones).flatten()[index_chart >= 0] # frequency 1
+    f2 = freqs[index_chart2].flatten() # frequency 2
+    t1 = (times.reshape(-1,1) * ones).flatten()[index_chart >= 0] # time 1
+    delta_t = (times[index_chart2].flatten() - t1) # change between time 2 and time 1
     # creds to anna
-    key_times = np.vstack((f1, f2, delta_t, t1)).T
+    key_times = np.vstack((f1, f2, delta_t, t1)).T.astype(int)
     # key_times = list(tuple(zip(map(tuple, key_times[:, :3]), key_times[:, 3]))) # had to get rid of the named tuple for formatting, to be fixed
     return key_times
