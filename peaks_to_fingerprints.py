09777d21c81d36dd4930352fde76920abbb43a74
New Annabelle <lewa@bxscience.edu>
2018-07-13 14:15:15 -0400

    final, optimized peaks to fingerprints method

--- peaks_to_fingerprints.py
+++ peaks_to_fingerprints.py
@@ -22,18 +22,20 @@ import numpy as np
             int. The keys are formatted as (f1, f2, t2-t1)."""
 def peaks_to_fingerprints(peaks, fanout):
     # gets two arrays, frequency and time. Use np.where on peaks
-    freqs, times = np.where(peaks)
+    times, freqs = np.where(peaks.T)
 
-    # creates list to return
-    key_times = list()
+    frequency_index = np.arange(len(freqs)) # makes an array for every index in data(i: 0,1,2,3,4,5,6)
+    index_chart = frequency_index[:-1].reshape(-1, 1) + (np.arange(fanout) + 1) # makes a chart of all the indexes, row = f2 values associated with f1 (col)
+    index_chart = index_chart[index_chart < len(frequency_index)] # deletes indicies that don't exist
 
-    # for loop for fanout -- OPTIMIZED NOW, BLESS YOU ANNA
-    for j in range(1, fanout):
-        f1 = freqs[:len(freqs) - j] # frequency 1
-        f2 = freqs[j:] # frequency 2
-        delta_t = times[j:] - times[:len(times) - j] # change between time 2 and time 1
-        t1 = times[:len(times) - j] # time 1
-        # OMG BLESS YOU ANNA YOU MANAGED TO MAKE THIS OPTIMIZED
-        key_times = np.vstack((f1, f2, delta_t, t1)).T
-        key_times = list(tuple(zip(map(tuple, key_times[:,:3]), key_times[:, 3]))) # had to get rid of the named tuple for formatting, to be fixed
+    # this was like 90% edison so creds to him
+    ones = np.ones(fanout, dtype=int)
+    # f1 = (freqs.reshape(-1,1) * ones)[:index_chart].flatten()[:len(index_chart)] # frequency 1
+    print((freqs.reshape(-1, 1) * ones)[:, :index_chart])
+    f2 = freqs[index_chart].flatten() # frequency 2
+    t1 = (times.reshape(-1,1) * ones[:index_chart]).flatten()[:len(index_chart)] # time 1
+    delta_t = (times[index_chart].flatten() - t1) # change between time 2 and time 1
+    # creds to anna
+    key_times = np.vstack((f1, f2, delta_t, t1)).T
+    # key_times = list(tuple(zip(map(tuple, key_times[:, :3]), key_times[:, 3]))) # had to get rid of the named tuple for formatting, to be fixed
     return key_times
