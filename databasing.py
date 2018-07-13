021ae9e7e7a492712d56160b2140a7866cef9e5b
awesomelemonade <awesomelemonade@gmail.com>
2018-07-13 10:41:46 -0400

    Added docstrings to databasing

--- databasing.py
+++ databasing.py
@@ -4,24 +4,52 @@ from collections import Counter, namedtuple
 
 class AudioDatabase:
 	def __init__(self, filename):
+		"""Initializes with a filename for the database to store to
+		Parameters
+		----------
+			filename: String
+				A String representing the file that it is supposed to store to
+		"""
 		self.filename = filename;
 		self.dictionary = {}
 	def put(self, key, value):
+		"""Puts a pair of key and value into the dictionary"""
 		if key not in self.dictionary:
 			self.dictionary[key] = []
 		self.dictionary[key].append(value)
 	def get(self, key):
+		"""Retrieves value based off key"""
 		return self.dictionary.get(key, [])
-	def save(self):	
+	def save(self):
+		"""Saves dictionary into file"""
 		with open(self.filename, mode="wb") as file:
 			pickle.dump(self.dictionary, file)
 	def load(self):
+		"""Loads dictionary from file"""
 		with open(self.filename, mode="rb") as file:
 			self.dictionary = pickle.load(file)
 	def store(self, fingerprints, song):
+		"""Stores fingerprints with a song id to dictionary
+		Parameters
+		----------
+			fingerprints: List
+				List of "Fingerprint" named tuples
+			song: integer
+				An integer representing the song id
+		"""
 		for fingerprint in fingerprints:
 			self.put((fingerprint.frequency1, fingerprint.frequency2, fingerprint.delta), (song, fingerprint.time))
 	def predict(self, fingerprints):
+		"""Predicts a song id based off stored dictionary and fingerprints
+		Parameters
+		----------
+			fingerprints: List
+				List of "Fingerprint" named tuples
+		Returns
+		-------
+			songId: integer
+				An integer representing the song id
+		"""
 		counter = Counter()
 		for fingerprint in fingerprints:
 			matches = self.get((fingerprint.frequency1, fingerprint.frequency2, fingerprint.delta))
