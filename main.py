c993d99ffb6636ea4244612e9a3337a9b9e7ddf2
Edison Situ <situe@bxscience.edu>
2018-07-13 16:31:47 -0400

    Random tests

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import databasing
 import peaks_to_fingerprints
 import Digital_To_Spectrum
 import digital_signal
+import numpy as np
 
 
 import matplotlib.pyplot as plt
@@ -10,18 +11,20 @@ import matplotlib.mlab as mlab
 def storeMP3toDatabase(directory, songId):
 	data, sampling_rate = digital_signal.get_mp3_data(directory)
 	spectrogram = Digital_To_Spectrum.dig_to_spec(data, sampling_rate)
-	threshold = np.percentile(spectrogram, 90)
+	threshold = np.percentile(spectrogram, 30)
 	peaks = Digital_To_Spectrum.spec_to_peaks(spectrogram, threshold)
 	print(threshold)
 	fingerprints = peaks_to_fingerprints.peaks_to_fingerprints(peaks, 15)
 	print(fingerprints.shape)
 	database = databasing.AudioDatabase("database.whydoyoucareaboutthisextension")
+	database.load()
 	database.store(fingerprints, songId)
 	database.save()
 def micMatch(duration):
 	data, sampling_rate = digital_signal.get_microphone_data(duration)
 	spectrogram = Digital_To_Spectrum.dig_to_spec(data, sampling_rate)
-	peaks = Digital_To_Spectrum.spec_to_peaks(spectrogram, 0)
+	threshold = np.percentile(spectrogram, 30)
+	peaks = Digital_To_Spectrum.spec_to_peaks(spectrogram, threshold)
 	fingerprints = peaks_to_fingerprints.peaks_to_fingerprints(peaks, 15)
 	print(fingerprints.shape)
 	database = databasing.AudioDatabase("database.whydoyoucareaboutthisextension")
@@ -34,13 +37,15 @@ def load():
 import os
 
 directory = "./music/"
-#for index, filename in enumerate(os.listdir(directory)):
-#	print("Storing MP3: "+filename+" - "+str(index))
-#	storeMP3toDatabase(directory + filename, index)
+for index, filename in enumerate(os.listdir(directory)):
+	print("Storing MP3: "+filename+" - "+str(index))
+	storeMP3toDatabase(directory + filename, index)
+	if index == 5:
+		break
 
-storeMP3toDatabase("./music/taco.mp3", 0)
+#storeMP3toDatabase("./music/taco.mp3", 0)
 
 #load()
 
-#micMatch(10)
+micMatch(10)
 
